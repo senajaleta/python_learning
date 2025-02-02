c359c46c4c430d509956c101bae412675eb11b1e
Sena Jaleta <senajaleta2011@gmail.com>
2025-02-02 05:43:03 +0300

    Python Directory and Files Management

---
+++ OOP/my_note1.py
@@ -0,0 +1,16 @@
+import os
+
+print(os.getcwd()) #get the present working directory using the getcwd(
+os.chdir('path')#change the current working directory by using the chdir() method.
+print(os.listdir())#All files and sub-directories inside a directory can be retrieved using the listdir() method.
+os.mkdir('try1')#make a new directory using the mkdir() method.
+os.rename('old one','new_one')#  takes in two basic arguments
+os.remove("myfile.txt")#used the remove() method to remove the "myfile.txt" file.
+os.rmdir("mydir") #use rmdir() to delete an empty directory,
+#Python Directory and Files Management
+
+
+
+
+
+
