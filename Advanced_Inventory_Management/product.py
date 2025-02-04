8576796e842d95247b5c9aa5aa01d4aad3aaee7d
Sena Jaleta <senajaleta2011@gmail.com>
2025-02-04 22:24:42 +0300

    Creating the base class

---
+++ Advanced_Inventory_Management/product.py
@@ -0,0 +1,13 @@
+
+
+
+class Product:
+    def __init__(self,Id,name,price,category,quantity):
+        self.id = id
+        self.name = name
+        self.price = price
+        self.category = category
+        self.quantity = quantity
+
+
+
